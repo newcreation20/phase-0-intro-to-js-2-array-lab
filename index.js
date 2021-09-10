00d3b40da60ab22e4e900b941b2681dabf0a019a
Kerny <kerny14@gmail.com>
2021-09-10 15:41:18 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+  function destructivelyAppendCat(Ralph) {
+    cats.push('Ralph');
+  }
+  
+  beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+  function destructivelyPrependCat(Bob) {
+      cats.unshift('Bob');
+  }
+  beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  }); 
+
+  function destructivelyRemoveLastCat() {
+      cats.pop('Garfield');
+  }
+  beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  }); 
+
+  function destructivelyRemoveFirstCat() {
+      cats.shift('Milo');
+  }
+  beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  }); 
+
+  function appendCat(Broom) {
+      //const copyOfCats = cats.slice();
+      cats.push(Broom);
+  }
